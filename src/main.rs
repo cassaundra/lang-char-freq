d3af22995de9acdea8a43b03c82732f4ba0f097b
Cassaundra <cass@cassaundra.io>
2020-05-14 12:02:59 -0700

    Continue work on program flow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use flate2::read::GzDecoder;
 use tar::Archive;
 
-use std::collections::BTreeMap;
 use std::fs::File;
+use std::io::ErrorKind;
+use std::path::PathBuf;
 
 mod analyze;
 pub use analyze::*;
@@ -17,27 +18,51 @@ pub mod github;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let config = Config::from_file(&PathBuf::from("config.toml"))?;
+
     let tmp_dir = std::env::temp_dir().join("lang-char-freq");
     if !tmp_dir.exists() {
         std::fs::create_dir(&tmp_dir)?;
     }
 
-    let search = github::search("Rust").await?;
+    let languages: Vec<&Language> = vec!["javascript", "typescript"].into_iter().map(|id| &config.languages[&String::from(id)]).collect();
 
-    for repo in search.repositories.iter().skip(2).take(4) {
-        println!("downloading repo {}", repo.full_name);
-        let archive = repo.download_archive(&tmp_dir).await?;
-        println!("extracting");
-        let archive = GzDecoder::new(File::open(archive)?);
-        let mut archive = Archive::new(archive);
-        archive.unpack(&tmp_dir)?;
+    for language in languages {
+        let search = github::search(&language.id).await?;
+
+        let repos = search
+            .repositories
+            .iter()
+            .filter(|repo| !language.exclude_repos.contains(&repo.full_name))
+            .take(config.repo_count);
+
+        // TODO skip existing repositories
+        for repo in repos {
+            println!("Repository: {}", repo.full_name);
+
+            // download
+            println!("  Downloading ({} bytes)...", repo.size);
+            let archive = repo.download_archive(&tmp_dir).await?;
+
+            // extract
+            println!("  Extracting...");
+            let archive = GzDecoder::new(File::open(archive)?);
+            let mut archive = Archive::new(archive);
+            match archive.unpack(&tmp_dir) {
+                Ok(_) => {}
+                Err(err) => match err.kind() {
+                    ErrorKind::AlreadyExists => println!("    Archive already exists, skipping."),
+                    _ => return Result::Err(Error::Io(err)),
+                },
+            }
+        }
+
+        println!("Analyzing all repositories");
+        let results = analyze_files(&tmp_dir, "rust", &config);
+        let mut results = results.unwrap().into_iter().collect::<Vec<(char, usize)>>();
+        results.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+        println!("{:?}", results);
     }
-
-    println!("ANALYZING");
-    let results = analyze_files(&tmp_dir, "rust", &Config::from_file(&std::path::PathBuf::from("config.toml")).unwrap());
-    let mut results = results.unwrap().into_iter().collect::<Vec<(char, usize)>>();
-    results.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-    println!("{:#?}", results);
 
     Ok(())
 }
